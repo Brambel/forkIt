6ee850929d557d4c3b6e16f0264ab7610fbe22d7
Brambel <jamesebert1@gmail.com>
2016-01-21 10:40:49 -0600

    added main functionality

--- forkIt/forkIt.cpp
+++ forkIt/forkIt.cpp
@@ -2,10 +2,65 @@
 //
 
 #include "stdafx.h"
+#include "iostream"
+#include <string>
 
+#define sizeLim 10
+using namespace std;
+
+struct arguments
+{
+	int argc;
+	char **argv;
+};
+
+arguments* readPrompt();
+arguments* writePrompt();
+void testArgs(arguments*);
 
 int main()
 {
+	arguments * temp = writePrompt();
+	testArgs(temp);
+	system("pause");
     return 0;
 }
 
+arguments* readPrompt() {
+	arguments* temp = new arguments;
+	string rawInput;
+	getline(std::cin,rawInput);
+	temp->argc = 1;
+	char* ptr = new char[rawInput.length()+1];
+	
+	char* head = ptr;   //create unchanging head refrence  so we don't advance as we create new array
+	temp->argv = (&head);
+
+	for (int i = 0; i < rawInput.length()+1; ++i, ++ptr) {//need to catch terminating null
+		if (rawInput[i] == ' ') {
+			rawInput[i] = NULL;
+			temp->argc += 1;
+		}
+		(*ptr) = rawInput[i];
+	}
+
+	return temp;
+}
+
+arguments* writePrompt() {
+	cout << ">";
+	return readPrompt();
+}
+
+void testArgs(arguments* args) { //just tests using our args class
+
+	char* ptr = (*args->argv);
+
+	for (int i = 0; i < args->argc; ++i, ++ptr) {
+		while ((*ptr) != NULL) {
+			cout << (*ptr);
+			++ptr;
+		}
+		cout << '\n';
+	}
+}
